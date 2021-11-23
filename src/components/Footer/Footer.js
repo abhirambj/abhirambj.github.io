d78502df1867b07fd8c0c3faef4f3e491fe78fb2
Abhiram B J <54413769+abhirambj@users.noreply.github.com>
2021-11-23 12:08:56 +0530

    final

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href='https://abhirambj.me' className='link footer__link'>
+      Created By Abhiram B J
+    </a>
+  </footer>
+)
+
+export default Footer
